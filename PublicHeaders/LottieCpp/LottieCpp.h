a540e0d91a9cbe91968798310d515496c85bc043
Isaac <>
2024-06-04 19:16:14 +0400

    Initial commit

---
+++ PublicHeaders/LottieCpp/LottieCpp.h
@@ -0,0 +1,13 @@
+#ifndef LottieCpp_h
+#define LottieCpp_h
+
+#import <LottieCpp/RenderTreeNode.h>
+#import <LottieCpp/CGPath.h>
+#import <LottieCpp/Vectors.h>
+#import <LottieCpp/Color.h>
+#import <LottieCpp/ShapeAttributes.h>
+#import <LottieCpp/PathElement.h>
+#import <LottieCpp/CurveVertex.h>
+#import <LottieCpp/BezierPath.h>
+
+#endif /* LottieCpp_h */
