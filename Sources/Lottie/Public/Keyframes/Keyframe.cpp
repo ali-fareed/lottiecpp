d32a4afe5e196ab22a9e11b9b0318091f10e2707
Isaac <>
2024-06-11 12:25:01 +0400

    Upgrade cubic solver

--- Sources/Lottie/Public/Keyframes/Keyframe.cpp
+++ Sources/Lottie/Public/Keyframes/Keyframe.cpp
@@ -7,35 +7,39 @@
 
 namespace lottie {
 
-static float eval_poly(float t, float b) { return b; }
-
 static float eval_poly(float t, float m, float b) {
-    return eval_poly(t, std::fmaf(m, t, b));
+    return std::fmaf(m, t, b);
 }
 
 static float eval_poly(float t, float m, float b, float c) {
-    return eval_poly(t, std::fmaf(m, t, b), c);
+    float a = std::fmaf(m, t, b);
+    return std::fmaf(t, a, c);
 }
 
 static float eval_poly(float t, float m, float b, float c, float d) {
-    return eval_poly(t, std::fmaf(m, t, b), c, d);
+    float e = std::fmaf(m, t, b);
+    float a = std::fmaf(e, t, c);
+    return std::fmaf(t, a, d);
 }
 
-__unused static float cubic_solver(float A, float B, float C, float D) {
+static float cubic_solver(float A, float B, float C, float D) {
     float t = -D;
+    
+    float A3 = 3.0f * A;
+    float A6 = 6.0f * A;
+    float B2 = 2.0f * B;
 
-    int iters = 0;
     const int MAX_ITERS = 8;
-    for (; iters < MAX_ITERS; ++iters) {
-        float f = eval_poly(t, A, B, C, D);        // f   = At^3 + Bt^2 + Ct + D
+    for (int iters = 0; iters < MAX_ITERS; iters++) {
+        float f = eval_poly(t, A, B, C, D); // f = At^3 + Bt^2 + Ct + D
         if (std::fabs(f) <= 0.00005f) {
             break;
         }
-        float fp = eval_poly(t, 3*A, 2*B, C);      // f'  = 3At^2 + 2Bt + C
-        float fpp = eval_poly(t, 3*A + 3*A, 2*B);  // f'' = 6At + 2B
+        float fp = eval_poly(t, A3, B2, C); // f' = 3At^2 + 2Bt + C
+        float fpp = eval_poly(t, A6, B2); // f'' = 6At + 2B
 
-        float numer = 2 * fp * f;
-        float denom = std::fma(2 * fp, fp, -(f * fpp));
+        float numer = 2.0f * fp * f;
+        float denom = std::fma(2.0f * fp, fp, -(f * fpp));
 
         t -= numer / denom;
     }
@@ -45,93 +49,8 @@ __unused static float cubic_solver(float A, float B, float C, float D) {
     return t;
 }
 
-__unused static inline bool nearly_zero(float x) {
-#ifdef DEBUG
-    assert(x >= 0);
-#endif
-    return x <= 0.0000000001f;
-}
-
-static float cubicRoot(float value) {
-    return pow(value, 1.0 / 3.0);
-}
-
-static float SolveQuadratic(float a, float b, float c) {
-    float result = (-b + sqrt((b * b) - 4 * a * c)) / (2 * a);
-    if (isInRangeOrEqual(result, 0.0, 1.0)) {
-        return result;
-    }
-    
-    result = (-b - sqrt((b * b) - 4 * a * c)) / (2 * a);
-    if (isInRangeOrEqual(result, 0.0, 1.0)) {
-        return result;
-    }
-    
-    return -1.0;
-}
-
 inline bool isApproximatelyEqual(float value, float other) {
     return std::abs(value - other) <= FLT_EPSILON;
-}
-
-__unused static float SolveCubic(float a, float b, float c, float d) {
-    if (isApproximatelyEqual(a, 0.0f)) {
-        return SolveQuadratic(b, c, d);
-    }
-    if (isApproximatelyEqual(d, 0.0f)) {
-        return 0.0;
-    }
-    b /= a;
-    c /= a;
-    d /= a;
-    float q = (3.0 * c - (b * b)) / 9.0;
-    float r = (-27.0 * d + b * (9.0 * c - 2.0 * (b * b))) / 54.0;
-    float disc = (q * q * q) + (r * r);
-    float term1 = b / 3.0;
-    
-    if (disc > 0.0) {
-        float s = r + sqrt(disc);
-        s = (s < 0) ? -cubicRoot(-s) : cubicRoot(s);
-        float t = r - sqrt(disc);
-        t = (t < 0) ? -cubicRoot(-t) : cubicRoot(t);
-        
-        float result = -term1 + s + t;
-        if (isInRangeOrEqual(result, 0.0, 1.0)) {
-            return result;
-        }
-    } else if (isApproximatelyEqual(disc, 0.0f)) {
-        float r13 = (r < 0) ? -cubicRoot(-r) : cubicRoot(r);
-        
-        float result = -term1 + 2.0 * r13;
-        if (isInRangeOrEqual(result, 0.0, 1.0)) {
-            return result;
-        }
-        
-        result = -(r13 + term1);
-        if (isInRangeOrEqual(result, 0.0, 1.0)) {
-            return result;
-        }
-    } else {
-        q = -q;
-        float dum1 = q * q * q;
-        dum1 = acos(r / sqrt(dum1));
-        float r13 = 2.0 * sqrt(q);
-        
-        float result = -term1 + r13 * cos(dum1 / 3.0);
-        if (isInRangeOrEqual(result, 0.0, 1.0)) {
-            return result;
-        }
-        result = -term1 + r13 * cos((dum1 + 2.0 * M_PI) / 3.0);
-        if (isInRangeOrEqual(result, 0.0, 1.0)) {
-            return result;
-        }
-        result = -term1 + r13 * cos((dum1 + 4.0 * M_PI) / 3.0);
-        if (isInRangeOrEqual(result, 0.0, 1.0)) {
-            return result;
-        }
-    }
-    
-    return -1.0;
 }
 
 float cubicBezierInterpolate(float value, Vector2D const &P1, Vector2D const &P2) {
@@ -147,11 +66,7 @@ float cubicBezierInterpolate(float value, Vector2D const &P1, Vector2D const &P2
         float b = -6 * P1.x + 3 * P2.x;
         float c = 3 * P1.x;
         
-#if 1
         float tTemp = cubic_solver(a, b, c, -value);
-#else
-        float tTemp = SolveCubic(a, b, c, -value);
-#endif
         
         if (isApproximatelyEqual(tTemp, -1.0f)) {
             return -1.0;
